6a7cd78dd11bcb8e1b6bf0ddf24d9fc3a107f0e8
Logan Miller <millerl1960@gmail.com>
2023-05-31 19:26:23 -0700

    Refactor main module

--- main.py
+++ main.py
@@ -7,74 +7,85 @@ from utils.trading_utils import calculate_cumulative_score, execute_trade, calcu
 from config import TICKERS
 import heapq
 
-def main():
-    logging.basicConfig(filename='logs/trading_bot.log', level=logging.INFO)
-    logging.info("Starting trading bot")
 
-    ticker_data = {}  # The dictionary to store information for each ticker
+def process_ticker(ticker, trade_period):
+    logging.info(f"Processing ticker {ticker}")
 
-    trade_period = get_current_market_period()
+    # Get headlines for the ticker
+    try:
+        headlines = get_headlines(ticker, trade_period['headline_start_time'])
+        logging.info(f"Found {len(headlines)} headlines for {ticker}")
+    except:
+        logging.info(f"Error getting headlines for {ticker}, continuing.")
+        return None
 
-    for ticker in TICKERS:
-        logging.info(f"Processing ticker {ticker}")
+    #skip this loop if no headlines returned
+    if not headlines:
+        return None
 
-        # Get headlines for the ticker
-        try:
-            headlines = get_headlines(ticker, trade_period['headline_start_time'])
-            logging.info(f"Found {len(headlines)} headlines for {ticker}")
-        except:
-            logging.info(f"Error getting headlines for {ticker}, continuing.")
-            continue
+    # Preprocess the headlines
+    headlines = preprocess_headlines(headlines)
+    logging.info(f"After preprocessing, {len(headlines)} headlines left for {ticker}")
 
-        #skip this loop if no headlines returned
-        if not headlines:
-            continue
+    records = []
 
-        # Preprocess the headlines
-        headlines = preprocess_headlines(headlines)
-        logging.info(f"After preprocessing, {len(headlines)} headlines left for {ticker}")
+    # Generate prompt and get GPT-3's response for each headline
+    for headline in headlines:
+        prompt = generate_prompt(headline, ticker)
+        response = get_gpt3_response(prompt)
+        score = process_gpt3_response(response)
 
-        records = []
+        # Store the headline, response, and score as a record
+        records.append({
+            "headline": headline,
+            "response": response,
+            "score": score
+        })
 
-        # Generate prompt and get GPT-3's response for each headline
-        for headline in headlines:
-            prompt = generate_prompt(headline, ticker)
-            response = get_gpt3_response(prompt)
-            score = process_gpt3_response(response)
+    # Calculate average score for the day
+    total_score = calculate_cumulative_score([record["score"] for record in records])
+    average_score = calculate_average_score([record["score"] for record in records])
+    logging.info(f"Average score for {ticker}: {average_score}, {total_score}")
 
-            # Store the headline, response, and score as a record
-            records.append({
-                "headline": headline,
-                "response": response,
-                "score": score
-            })
+    # Store all the relevant information for the ticker in the dictionary
+    ticker_data = {
+        "records": records,
+        "average_score": average_score,
+        "total_score": total_score,
+        "buy_time": trade_period['trade_buy_time'],
+        "sell_time": trade_period['trade_sell_time']
+    }
 
-        # Calculate average score for the day
-        total_score = calculate_cumulative_score([record["score"] for record in records])
-        average_score = calculate_average_score([record["score"] for record in records])
-        logging.info(f"Average score for {ticker}: {average_score}, {total_score}")
+    if average_score < 0:
+        with open('data/negative_averages.json', 'w') as outfile:
+            json.dump({ticker: ticker_data}, outfile, indent=4, default=str)
 
-        # Store all the relevant information for the ticker in the dictionary
-        ticker_data[ticker] = {
-            "records": records,
-            "average_score": average_score,
-            "total_score": total_score,
-            "buy_time": trade_period['trade_buy_time'],
-            "sell_time": trade_period['trade_sell_time']
-        }
-
-        if average_score < 0:
-            with open('data/negative_averages.json', 'w') as outfile:
-                json.dump({ticker: data for ticker, data in ticker_data.items() if data['average_score'] < 0}, outfile, indent=4, default=str)
+    return ticker_data
 
 
-    logging.info("Finished processing all tickers")
-
+def execute_trades(ticker_data):
     # Now execute trades based on the three tickers with the lowest average scores
     worst_tickers = heapq.nsmallest(3, ticker_data.items(), key=lambda x: x[1]["average_score"])
     for ticker, data in worst_tickers:
         logging.info(f"Executing trade for {ticker} with average score {data['average_score']} and trade time {data['trade_time']}")
         execute_trade(ticker, data)
+
+
+def main():
+    logging.basicConfig(filename='logs/trading_bot.log', level=logging.INFO)
+    logging.info("Starting trading bot")
+
+    ticker_data = {}  # The dictionary to store information for each ticker
+    trade_period = get_current_market_period()
+
+    for ticker in TICKERS:
+        ticker_info = process_ticker(ticker, trade_period)
+        if ticker_info:
+            ticker_data[ticker] = ticker_info
+
+    logging.info("Finished processing all tickers")
+    execute_trades(ticker_data)
+
 
 if __name__ == "__main__":
     main()
